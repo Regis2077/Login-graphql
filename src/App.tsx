912b4fcaad459a5a1e53ab9ca0f040e06b99b55a
regis <gbrregispf@gmail.com>
2022-11-22 16:00:43 -0300

    feat: apollo provider and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Header } from './components';
+import { ApolloProvider } from '@apollo/client';
+import { client } from './graphql';
+import { Home, Login } from './pages';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <ApolloProvider client={client}>
+        <Header />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />}>
+            </Route>
+            <Route path="/home" element={<Home/>}>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ApolloProvider>
+        
     </div>
   );
 }
-
 export default App;
