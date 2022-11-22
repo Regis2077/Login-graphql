d6e3a3b7e268fbeae03a980d51c3426644ce2e75
regis <gbrregispf@gmail.com>
2022-11-22 16:03:37 -0300

    feat: header

---
+++ src/components/header/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { ContainerHeader } from './styles.js'
+
+
+export const Header = () => {
+  return (
+    <ContainerHeader>Academy <span>Store</span></ContainerHeader>
+  )
+}
---
+++ src/components/header/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const ContainerHeader = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  background-color: #17092C;
+  height: 64px;
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 32px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: #FFFFFF;
+  span{
+    color: #52B45B
+  }
+`
